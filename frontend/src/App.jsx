25225a3e9b2ab86c87c5812d38ab9192985e454a
salaudeenegov <salaudeen.n@egovernments.org>
2025-06-26 15:24:46 +0530

    boiler code plate is reddy

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./App.css"
+import { Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home'
+import LoginPage from './Pages/LoginPage'
+import RegisterPage from './Pages/RegisterPage'
+
+const App = () => {
+  return (
+ 
+   <Routes>
+    <Route path='/' element={<LoginPage/>}/>
+    <Route path='/register' element={<RegisterPage/>}/>
+       <Route path='/home' element={<Home/>}/>
+
+
+   </Routes>
+ 
+  )
+}
+
+export default App
