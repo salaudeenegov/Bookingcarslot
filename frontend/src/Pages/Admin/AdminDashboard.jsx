a3c2ad44df82c005fe276e0d5af0dbbaba3834e0
salaudeenegov <salaudeen.n@egovernments.org>
2025-06-27 11:47:34 +0530

    restructed the files

--- frontend/src/Pages/Admin/AdminDashboard.jsx
+++ frontend/src/Pages/Admin/AdminDashboard.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useParking } from '../context/ParkingContext';
+import { useParking } from '../../context/ParkingContext';
 
 const AdminDashboard = () => {
   const { slots, addSlots, loading } = useParking();
